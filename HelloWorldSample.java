2e4c54770630445231027d370d5682f3cee6e339
CrazyRichMonke <99268999+CrazyRichMonke@users.noreply.github.com>
2022-09-09 08:41:58 -0400

    Update HelloWorldSample.java

---
+++ HelloWorldSample.java
@@ -0,0 +1,13 @@
+/**   
+Subhy Alam 
+9/9/22
+Tappan Zee High School
+*/
+
+public class HelloWorldSample{
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        System.out.println("Hooray! I did it.");
+
+    }
+}
