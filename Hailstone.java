a38dd6400603c6b886f882e29834d5d4eed03cdd
CrazyRichMonke <99268999+CrazyRichMonke@users.noreply.github.com>
2022-10-11 08:20:12 -0400

    s

---
+++ Hailstone.java
@@ -0,0 +1,19 @@
+public class Hailstone{
+    public static void main(String[]args){
+        int startNum = 1;
+        int numOfCycles = 0;
+        while (startNum <= 200){
+            int endNum = startNum;
+            do {
+                if(endNum % 2 == 0){
+                    endNum /= 2;
+                }else{
+                    endNum =  (endNum * 3)+ 1;
+                }
+                numOfCycles ++;
+            } while (endNum != 1);
+            System.out.println("There is " +numOfCycles+ " cycles");
+            startNum++;
+        }
+    }
+}
